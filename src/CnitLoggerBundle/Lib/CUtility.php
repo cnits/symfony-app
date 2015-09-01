3945fce667bba73f6f2125202f05e8e922e26218
phonglam <phonglam@ccintegration.com>
2015-09-01 09:02:35 +0700

    Logger - Update code

--- src/CnitLoggerBundle/Lib/CUtility.php
+++ src/CnitLoggerBundle/Lib/CUtility.php
@@ -23,7 +23,7 @@ final class CUtility {
 
     public static function getException($ex, $logType = "LogType"){
         $mLogger = new \Monolog\Logger($logType);
-        $mLogger -> pushHandler(new \Monolog\Handler\StreamHandler($logType.".log", \Monolog\Logger::ERROR));
+        $mLogger -> pushHandler(new \Monolog\Handler\StreamHandler("app/logs/".$logType.".log", \Monolog\Logger::ERROR));
         if($ex instanceof \Exception){
             $mLogger -> addError(json_encode($ex));
         }else{
