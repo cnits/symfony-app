2a472ee86319f9c1518a03708e0336ae6248347f
phongnhung <appstoregugle2310@gmail.com>
2016-01-09 16:34:08 +0700

    test web socket

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Gos\Bundle\WebSocketBundle\GosWebSocketBundle(),
+            new Gos\Bundle\PubSubRouterBundle\GosPubSubRouterBundle(),
             new CnitLoggerBundle\CnitLoggerBundle(),
             new CnitSnappyPDFBundle\CnitSnappyPDFBundle(),
             new CnitCrmBundle\CnitCrmBundle(),
